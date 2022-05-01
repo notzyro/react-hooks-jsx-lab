ae826647b15cfb3ce97e82cecc23b9c87b31c7cf
Edward Angelo Salas <zyro670@gmail.com>
2022-05-01 17:23:35 -0400

    commit tree 9c2667f606025a39d9a8830adf955f8b14e26693

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
+export function About() {
+  return <div id="about">About
+  <h2>About Me</h2>
+  <p>Test Content</p>
+  <img src={image} alt="I made this" />
+  </div>;
 }
 
 export default About;
